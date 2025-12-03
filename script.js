ed5bf62ed5b72d7b27305f68178c9a378abc797a
SanikaMotegaonkar <sanika2586@gmail.com>
2025-12-03 10:09:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+  alert("Login feature coming soon!");
+});
+
+document.getElementById("registerBtn").addEventListener("click", () => {
+  alert("Register feature coming soon!");
+});
